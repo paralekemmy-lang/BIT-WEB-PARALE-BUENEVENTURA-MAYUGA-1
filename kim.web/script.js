dcbe87f870c0e3840c08989979b88bf6aee5b76b
paralekemmy-lang <paralekemmy@gmail.com>
2025-10-23 09:02:51 +0800

    Add files via upload

---
+++ kim.web/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const links = document.querySelectorAll("nav a");
+
+    links.forEach(link => {
+        link.addEventListener("click", event => {
+        event.preventDefault(); 
+        const sectionId = link.getAttribute("data-section");
+        document.getElementById(sectionId).scrollIntoView({
+            behavior: "smooth"
+            });
+        });
+    });
+});
